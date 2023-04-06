6e4f76deee7c50bcf93000041d26f0b025ba6868
komshuk <88716968+komshuk@users.noreply.github.com>
2023-04-06 22:00:36 +0300

    Update CashbackHackService.java

--- src/main/java/ru/netology/CashbackHackService.java
+++ src/main/java/ru/netology/CashbackHackService.java
@@ -4,8 +4,6 @@ public class CashbackHackService {
     private final int boundary = 1000;
 
     public int remain(int amount) {
-        if (amount == 1000)
-           return 0;
         return boundary - amount % boundary;
     }
 }
